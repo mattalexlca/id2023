f2c3518fd48a1b9ac682170f9858a8d4ebc1228d
mattalexlca <127574084+mattalexlca@users.noreply.github.com>
2023-03-10 21:57:05 -0500

    Create bad.java

---
+++ bad.java
@@ -0,0 +1,8 @@
+public clas MyProgram {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 10;
+    int sum = x + y;
+    System.out.println("The sum of x and y is: " + sum)
+  }
+}
